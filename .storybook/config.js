2d11b21b085fbcdeadd8d70ef96f3649a10e9d55
Trevor Miranda <trevor.miranda@pm.me>
2019-01-27 21:12:22 -0500

    Update .storybook/config.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../stories', true, /.\.jsx$/)
+const req = require.context('../stories', true, /.\.jsx$/);
 
 configure(() => {
     req.keys().forEach(req);
