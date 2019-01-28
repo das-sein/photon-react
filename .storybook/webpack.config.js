b731e402774e8673680ca772bedce81fdccda36b
Trevor Miranda <trevor.miranda@pm.me>
2019-01-27 21:38:55 -0500

    Now that we're using a CDN to serve fonts, don't need storybook to care

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,41 +2,15 @@ const path = require('path');
 
 module.exports = (base, type) => {
     base.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf)$/,
-        use: [{
-            loader: 'file-loader',
-        }],
-        include: path.resolve(__dirname, '../node_modules/fira/')
-    });
-    base.module.rules.push({
         test: /\.scss$/,
-        use: [
-            {
-                loader: 'resolve-url-loader',
-                options: {
-                    absolute: true
-                }
-            },
-            {
-                loader: 'sass-loader',
-                options: {
-                    sourceMap: true,
-                    sourceMapContents: false
-                },
-            }
-        ],
+        use: 'sass-loader',
         include: path.resolve(__dirname, '../src')
     });
     base.resolve.extensions.push(
         '.js',
         '.jsx',
         '.ts',
-        '.tsx',
         '.scss',
-        '.woff',
-        '.woff2',
-        '.ttf',
-        '.eof'
     );
 
     return base;
