3d9151c5bb16561b576ae121a0c5fed9e34b6441
Trevor Miranda <trevor.miranda@pm.me>
2019-01-27 20:06:18 -0500

    Add CardActions to exported in main.ts

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import Button from './Button';
+import Typography from './Typography';
+import { Card, CardActions, CardHeader, CardBody } from './card';
+
+const card = {
+    Card,
+    CardActions,
+    CardBody,
+    CardHeader
+};
+
+export {
+    Button,
+    card,
+    Typography
+};
