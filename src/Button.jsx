d31a71a2891991ee2e8283a3336cefbf972d3b6c
Trevor Miranda <trevor.miranda@pm.me>
2019-01-28 16:30:21 -0500

    Adjust Button typography

--- src/Button.jsx
+++ src/Button.jsx
@@ -17,16 +17,18 @@ export default function Button(props) {
         }
     );
     // Figure out what size the caption should be
-    let typo_size = 'lg'; // Medium caption as default
+    let typo_size = 'md'; // Body-10 as default
     if (props.micro) {
         typo_size = 'sm';
+    } else if (props.puffy) {
+        typo_size = 'lg';
     }
     return (
         <button type={'button'} className={classes} {...props}>
             {!props.ghost &&
-             <Typography.Caption size={typo_size} container={'div'}>
+             <Typography.Body size={typo_size} container={'div'}>
                  {props.text}
-             </Typography.Caption>
+             </Typography.Body>
             }
             {props.ghost && props.children}
         </button>
