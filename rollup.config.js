f6a6bfad60512fde5f690d0482d5c2554208c246
Trevor Miranda <trevor.miranda@pm.me>
2019-01-27 20:04:43 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,22 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import sass from 'rollup-plugin-sass';
+import typescript from 'rollup-plugin-typescript';
 
 import pkg from './package.json';
 
 
 export default {
-    input: 'src/main.js',
+    input: 'src/main.ts',
     output: [
         { file: pkg.main, format: 'cjs' }
     ],
     external: ['react'],
     plugins: [
+        resolve(),
         commonjs(),
-        resolve()
+        sass({ insert: true }),
+        typescript(),
+
     ]
 };
